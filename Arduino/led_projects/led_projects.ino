929361cf06ca1d3f5cd9994f94a93e0eda67854a
Peixiang Liu <Peixiang.Liu@infineon.com>
2021-06-08 09:13:49 +0200

    [ADD] led_projects

---
+++ Arduino/led_projects/led_projects.ino
@@ -0,0 +1,21 @@
+int ledPin = 13;
+
+
+
+
+void setup() {
+  
+  pinMode(ledPin, OUTPUT);
+}
+
+
+void loop()
+{
+  
+    digitalWrite(ledPin, HIGH);
+    delay(200);
+   
+    digitalWrite(ledPin, LOW);
+    delay(200);
+    
+}
